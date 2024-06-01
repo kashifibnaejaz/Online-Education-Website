8181251da179bace1db0f0e0188a0c149ea97c47
Kashif Hussain <91010442+kashifibnaejaz@users.noreply.github.com>
2024-06-01 12:44:53 +0530

    Add files via upload

--- profile.php
+++ profile.php
@@ -1,25 +1,29 @@
 <?php
 
-include 'components/connect.php';
+   include '../components/connect.php';
 
-if(isset($_COOKIE['user_id'])){
-   $user_id = $_COOKIE['user_id'];
-}else{
-   $user_id = '';
-   header('location:login.php');
-}
+   if(isset($_COOKIE['tutor_id'])){
+      $tutor_id = $_COOKIE['tutor_id'];
+   }else{
+      $tutor_id = '';
+      header('location:login.php');
+   }
 
-$select_likes = $conn->prepare("SELECT * FROM `likes` WHERE user_id = ?");
-$select_likes->execute([$user_id]);
-$total_likes = $select_likes->rowCount();
+   $select_playlists = $conn->prepare("SELECT * FROM `playlist` WHERE tutor_id = ?");
+   $select_playlists->execute([$tutor_id]);
+   $total_playlists = $select_playlists->rowCount();
 
-$select_comments = $conn->prepare("SELECT * FROM `comments` WHERE user_id = ?");
-$select_comments->execute([$user_id]);
-$total_comments = $select_comments->rowCount();
+   $select_contents = $conn->prepare("SELECT * FROM `content` WHERE tutor_id = ?");
+   $select_contents->execute([$tutor_id]);
+   $total_contents = $select_contents->rowCount();
 
-$select_bookmark = $conn->prepare("SELECT * FROM `bookmark` WHERE user_id = ?");
-$select_bookmark->execute([$user_id]);
-$total_bookmarked = $select_bookmark->rowCount();
+   $select_likes = $conn->prepare("SELECT * FROM `likes` WHERE tutor_id = ?");
+   $select_likes->execute([$tutor_id]);
+   $total_likes = $select_likes->rowCount();
+
+   $select_comments = $conn->prepare("SELECT * FROM `comments` WHERE tutor_id = ?");
+   $select_comments->execute([$tutor_id]);
+   $total_comments = $select_comments->rowCount();
 
 ?>
 
@@ -29,74 +33,56 @@ $total_bookmarked = $select_bookmark->rowCount();
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>profile</title>
+   <title>Profile</title>
 
    <!-- font awesome cdn link  -->
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="../css/admin_style.css">
 
 </head>
 <body>
 
-<?php include 'components/user_header.php'; ?>
-
-<section class="profile">
+<?php include '../components/admin_header.php'; ?>
+   
+<section class="tutor-profile" style="min-height: calc(100vh - 19rem);"> 
 
    <h1 class="heading">profile details</h1>
 
    <div class="details">
-
-      <div class="user">
-         <img src="uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
+      <div class="tutor">
+         <img src="../uploaded_files/<?= $fetch_profile['image']; ?>" alt="">
          <h3><?= $fetch_profile['name']; ?></h3>
-         <p>student</p>
+         <span><?= $fetch_profile['profession']; ?></span>
          <a href="update.php" class="inline-btn">update profile</a>
       </div>
-
-      <div class="box-container">
-
+      <div class="flex">
          <div class="box">
-            <div class="flex">
-               <i class="fas fa-bookmark"></i>
-               <div>
-                  <h3><?= $total_bookmarked; ?></h3>
-                  <span>saved playlists</span>
-               </div>
-            </div>
-            <a href="#" class="inline-btn">view playlists</a>
+            <span><?= $total_playlists; ?></span>
+            <p>total playlists</p>
+            <a href="playlists.php" class="btn">view playlists</a>
          </div>
-
          <div class="box">
-            <div class="flex">
-               <i class="fas fa-heart"></i>
-               <div>
-                  <h3><?= $total_likes; ?></h3>
-                  <span>liked tutorials</span>
-               </div>
-            </div>
-            <a href="#" class="inline-btn">view liked</a>
+            <span><?= $total_contents; ?></span>
+            <p>total videos</p>
+            <a href="contents.php" class="btn">view contents</a>
          </div>
-
          <div class="box">
-            <div class="flex">
-               <i class="fas fa-comment"></i>
-               <div>
-                  <h3><?= $total_comments; ?></h3>
-                  <span>video comments</span>
-               </div>
-            </div>
-            <a href="#" class="inline-btn">view comments</a>
+            <span><?= $total_likes; ?></span>
+            <p>total likes</p>
+            <a href="contents.php" class="btn">view contents</a>
          </div>
-
+         <div class="box">
+            <span><?= $total_comments; ?></span>
+            <p>total comments</p>
+            <a href="comments.php" class="btn">view comments</a>
+         </div>
       </div>
-
    </div>
 
 </section>
 
-<!-- profile section ends -->
 
 
 
@@ -109,18 +95,11 @@ $total_bookmarked = $select_bookmark->rowCount();
 
 
 
-<!-- footer section starts  -->
 
-<footer class="footer">
 
-   &copy; copyright @ 2022 by <span>mr. web designer</span> | all rights reserved!
+<?php include '../components/footer.php'; ?>
 
-</footer>
+<script src="../js/admin_script.js"></script>
 
-<!-- footer section ends -->
-
-<!-- custom js file link  -->
-<script src="js/script.js"></script>
-   
 </body>
 </html>
